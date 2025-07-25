1d444648741146173276c03dd13f449924bd71cb
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-07-26 00:04:14 +0530

    Signal commit

--- src/Conditional/Two/TrafficSignal.java
+++ src/Conditional/Two/TrafficSignal.java
@@ -3,7 +3,7 @@ package Conditional.Two;
 public class TrafficSignal {
 	
 	public String signal(String sig) {
-		
+		//red -> stop , green-> go , yellow->slow
 		if(sig.equalsIgnoreCase("red")) {
 			return "You should stop";
 		} else if(sig.equalsIgnoreCase("green")) {
